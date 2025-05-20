870446f81c5c5efa10dad643611fdd38323b8b52
Jabba Laci <jabba.laci@gmail.com>
2025-05-20 16:14:00 +0200

    alphabet

--- functions/alphabet.fish
+++ functions/alphabet.fish
@@ -1,7 +1,17 @@
 function alphabet -d "Print the alphabet (lowercase, uppercase, digits, printable characters)"
-    echo "abcdefghijklmnopqrstuvwxyz"
-    echo "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    echo "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    echo '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
-    echo '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c'
+    pypy3 -c "
+import string
+
+print(string.ascii_lowercase)
+print(string.ascii_uppercase)
+print(string.ascii_letters)
+print(string.punctuation)
+print(string.printable.rstrip())  # non-visible whitespaces are removed from the end
+"
 end
+
+# "abcdefghijklmnopqrstuvwxyz"
+# "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+# '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c'
