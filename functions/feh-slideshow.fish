a883cb6ed1c1ff908d74a0a30a16c2accd0df6e2
Jabba Laci <jabba.laci@gmail.com>
2026-05-19 10:36:56 +0200

    feh update

--- functions/feh-slideshow.fish
+++ functions/feh-slideshow.fish
@@ -6,15 +6,16 @@ end
 false && begin  # note
 Meaning of the options:
 
-* `-D 5`:          5 sec. delay
+* `-D 4`:          4 sec. delay
 * `-F`:            fullscreen
 * `-Z`:            auto-zoom images to fit screen
+* `-z`:            randomize the file list
 end
 
 
 function feh-slideshow --argument sec -d "Start a slideshow with feh"
     if test -z "$sec"
-        set sec 5
+        set sec 4
     end
-	feh -D $sec -F -Z
+	feh -D $sec -F -Z -z
 end
